d44ea4c389354a3fd58d59fca0cc73c2ebaa0ad0
Danilok95432 <danila.dvoretskov@yandex.ru>
2024-11-07 15:35:51 +0300

    переделал контейнер

--- src/pages/videos-page/layout/videos-layout.tsx
+++ src/pages/videos-page/layout/videos-layout.tsx
@@ -1,22 +1,17 @@
 import React, { type FC } from 'react'
 
 import { Outlet } from 'react-router-dom'
-import { createBreakpoint } from 'react-use'
 
 import { Container } from 'src/UI/Container/Container'
 import { BreadCrumbs } from 'src/modules/bread-crumbs/bread-crumbs'
 import { PageContent } from 'src/components/page-content/page-content'
 
-import { DisplayBreakpoints } from 'src/helpers/consts'
-
-const useBreakPoint = createBreakpoint({ M: DisplayBreakpoints.Md, S: DisplayBreakpoints.Sm })
+import styles from './index.module.scss'
 
 export const VideosLayout: FC = () => {
-	const breakpoint = useBreakPoint()
-
 	return (
 		<>
-			<Container>
+			<Container className={styles.videosContainer}>
 				<BreadCrumbs
 					crumbsLinksMap={[
 						{
@@ -25,17 +20,10 @@ export const VideosLayout: FC = () => {
 						},
 					]}
 				/>
-				{breakpoint === 'M' && (
-					<PageContent>
-						<Outlet />
-					</PageContent>
-				)}
-			</Container>
-			{breakpoint !== 'M' && (
-				<PageContent $minHeight='auto' $margin='0 0 45px 0'>
+				<PageContent className={styles.videosPageContent}>
 					<Outlet />
 				</PageContent>
-			)}
+			</Container>
 		</>
 	)
 }
