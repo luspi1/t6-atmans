5928d3b427990284247086a36381df39aa615a0a
luspi1 <igbabin566@gmail.com>
2024-11-29 16:56:57 +0300

    видео на главной переделано в слайдер

--- src/pages/home-page/components/videotape-section/videotape-section.tsx
+++ src/pages/home-page/components/videotape-section/videotape-section.tsx
@@ -1,7 +1,10 @@
-import { type FC } from 'react'
+import { type FC, type RefObject, useRef } from 'react'
+import { type SwiperRef } from 'swiper/react/swiper-react'
 
 import cn from 'classnames'
+import { Swiper, SwiperSlide } from 'swiper/react'
 
+import { homeVideosSliderOptions } from 'src/pages/home-page/components/videotape-section/consts'
 import { Container } from 'src/UI/Container/Container'
 import { AppRoute } from 'src/routes/main-routes/consts'
 import { useGetHomeVideosQuery } from 'src/store/home/home.api'
@@ -9,11 +12,13 @@ import { FlexRow } from 'src/components/flex-row/flex-row'
 import { MainButton } from 'src/UI/MainButton/MainButton'
 import { VideoCard } from 'src/components/video-card/video-card'
 
+import { SliderBtns } from 'src/components/slider-btns/slider-btns'
+
 import styles from './index.module.scss'
 
 export const VideotapeSection: FC = () => {
 	const { data: videos } = useGetHomeVideosQuery(null)
-
+	const swiperRef: RefObject<SwiperRef> = useRef<SwiperRef>(null)
 	return (
 		<section className={cn(styles.videotapeSection, '_bordered')}>
 			<Container className={styles.videotapeTop}>
@@ -23,8 +28,15 @@ export const VideotapeSection: FC = () => {
 						Все видео
 					</MainButton>
 				</FlexRow>
-				<div className={styles.videosList}>
-					{videos?.map((videoItem) => <VideoCard key={videoItem.id} {...videoItem} />)}
+				<div className='slider-with-btns'>
+					<Swiper {...homeVideosSliderOptions} ref={swiperRef}>
+						{videos?.map((slideItem, idx) => (
+							<SwiperSlide key={idx}>
+								<VideoCard key={slideItem.id} {...slideItem} />
+							</SwiperSlide>
+						))}
+					</Swiper>
+					<SliderBtns $topPosition='40%' $btnsSpacing='calc(100% + 40px)' swiperRef={swiperRef} />
 				</div>
 			</Container>
 		</section>
